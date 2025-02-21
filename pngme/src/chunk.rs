72ac1e486da72eb3b158c2bacb82a4cba1e5257e
reedwx <jrp375@cornell.edu>
2025-02-21 01:04:14 -0800

    passing most test cases, need to fix try_from + data_as_string

--- pngme/src/chunk.rs
+++ pngme/src/chunk.rs
@@ -30,8 +30,56 @@ impl Chunk {
         self.length
     }
 
+    pub fn chunk_type(&self) -> &ChunkType {
+        &self.chunk_type
+    }
+
     pub fn crc(&self) -> u32 {
         self.crc
+    }
+
+    pub fn data(&self) -> &[u8] {
+        &self.chunk_data.as_slice()
+    }
+
+    pub fn data_as_string(&self) -> crate::Result<String> {
+        let mut v: Vec<u8> = Vec::new();
+        /*for byte in self.chunk_type.bytes().iter() {
+            v.push(*byte);
+        }*/
+        for byte in self.chunk_data.iter() {
+            v.push(*byte);
+        }
+        let string = String::from_utf8(v).expect("Our bytes should be valid utf8");
+        return Ok(string);
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.length
+            .to_be_bytes()
+            .iter()
+            .chain(self.chunk_type.bytes().iter())
+            .chain(self.crc.to_be_bytes().iter())
+            .copied()
+            .collect::<Vec<u8>>()
+        /*let type_bytes: &[u8] = self.chunk_type.bytes().iter();
+        let crc_bytes: &[u8] = self.crc.to_be_bytes().iter();
+        len.chain(type_bytes)
+            .chain(&self.chunk_data)
+            .chain(crc_bytes)
+            .collect::<Vec<u8>>()*/
+    }
+}
+
+impl fmt::Display for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Chunk {{",)?;
+        writeln!(f, "  Length: {}", self.length())?;
+        writeln!(f, "  Type: {}", self.chunk_type())?;
+        writeln!(f, "  Data: {} bytes", self.data().len())?;
+        writeln!(f, "  Crc: {}", self.crc())?;
+        writeln!(f, "}}",)?;
+        Ok(())
     }
 }
 
@@ -46,6 +94,16 @@ impl TryFrom<&[u8]> for Chunk {
             }));
         }
         let buf: [u8; 4] = <[u8; 4]>::try_from(&chunk[0..4]).unwrap();
+
+        //check if there's overflow
+        //process length as u64 then see if it's larger than max u32
+        let buf_copy: [u8; 8] = [0, 0, 0, 0, buf[0], buf[1], buf[2], buf[3]];
+        let big_len: u64 = u64::from_be_bytes(buf_copy);
+        if big_len > u32::MAX.into() {
+            return Err(Box::new(ChunkError {
+                err: "length is too big".to_owned(),
+            }));
+        }
         let len: u32 = u32::from_be_bytes(buf);
 
         //next four bytes are chunk type
@@ -134,7 +192,7 @@ mod tests {
         let chunk = testing_chunk();
         assert_eq!(chunk.chunk_type().to_string(), String::from("RuSt"));
     }
-    /*
+    //fails
     #[test]
     fn test_chunk_string() {
         let chunk = testing_chunk();
@@ -142,13 +200,13 @@ mod tests {
         let expected_chunk_string = String::from("This is where your secret message will be!");
         assert_eq!(chunk_string, expected_chunk_string);
     }
-
+    //fails
     #[test]
     fn test_chunk_crc() {
         let chunk = testing_chunk();
         assert_eq!(chunk.crc(), 2882656334);
     }
-
+    //fails
     #[test]
     fn test_valid_chunk_from_bytes() {
         let data_length: u32 = 42;
@@ -175,7 +233,7 @@ mod tests {
         assert_eq!(chunk_string, expected_chunk_string);
         assert_eq!(chunk.crc(), 2882656334);
     }
-
+    //fails
     #[test]
     fn test_invalid_chunk_from_bytes() {
         let data_length: u32 = 42;
@@ -216,5 +274,5 @@ mod tests {
         let chunk: Chunk = TryFrom::try_from(chunk_data.as_ref()).unwrap();
 
         let _chunk_string = format!("{}", chunk);
-        */
+    }
 }
